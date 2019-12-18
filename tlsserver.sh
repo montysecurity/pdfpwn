738ddcb4ddb784330ba939d7367d69cd9048262a
montyonsecurity <jacobcainmontgomery@gmail.com>
2019-12-17 20:14:27 -0500

    initial

---
+++ tlsserver.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+if [ "$EUID" -ne 0 ]
+then
+	echo -e "\e[91m[-]\e[0m Error: Permission Denied"
+	exit
+fi
+
+apt update && apt install apache2
+a2enmod ssl
+a2ensite default-ssl.conf
+service apache2 restart && echo -e "\e[92m[+]\e[0m TLS Server Properly Installed: /var/www/html"
